be364c0fde712ca9da39c0d60d6bc2407a4fd00e
Kamil Malinowski <malinowskikam96@gmail.com>
2024-11-28 23:10:39 +0100

    Added mouse definitions

---
+++ src/event.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum MouseButton {
+    Left,
+    Right,
+    Middle,
+}
+
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum ButtonState {
+    Up,
+    Down,
+}
+
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum InputEvent {
+    MouseButton{button: MouseButton, state: ButtonState},
+    MouseMoveAbs{x: f32, y: f32},
+    MouseMoveRel{x: f32, y: f32},
+}
