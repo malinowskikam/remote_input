0c0339d85fe72c626aff02f6f8c37cd510804fa2
Kamil Malinowski <malinowskikam96@gmail.com>
2024-11-28 23:11:32 +0100

    Added example implementation in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use remote_keyboard_mouse_server::server::Server;
+use tokio::sync::mpsc::channel;
+use remote_input::event::InputEvent;
+use remote_input::sink::{ConsoleSink, InputSink};
+use remote_input::source::{InputSource, VecSource, DevMiceSource};
 
-fn main() {
-    let s = Server::new(6789);
-    let t = s.listen();
+#[tokio::main]
+async fn main() {
+    let mut  events = vec![];
+    events.push(InputEvent::MouseMoveRel {x: 0.1, y: 0.0});
+    events.push(InputEvent::MouseMoveRel {x: 0.1, y: 0.1});
+    events.push(InputEvent::MouseMoveRel {x: 0.0, y: 0.1});
 
-    t.join().unwrap();
-    println!("Server stopped");
+    let (tx, rx) = channel(100);
+
+    // let mut source = VecSource::new(events);
+    let mut source = DevMiceSource::new();
+    let source_handle = tokio::spawn(async move {source.start_source(tx).await});
+
+    let mut sink = ConsoleSink::new();
+    let sink_handle = tokio::spawn(async move {sink.start_sink(rx).await});
+
+    let stop_handle = tokio::spawn(async move {
+        tokio::time::sleep(tokio::time::Duration::from_secs(9999)).await;
+        source_handle.abort();
+        sink_handle.abort();
+    });
+    
+    stop_handle.await.expect("Could not stop pipeline");
 }
